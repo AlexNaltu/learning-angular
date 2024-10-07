9e1d78429f3e099c08a1d112f0bedb0d65182061
Alex Naltu <naltuok@gmail.com>
2024-10-07 09:01:29 +0200

    for...of loops

--- learning-angular/src/app/components/admin/admin.component.ts
+++ learning-angular/src/app/components/admin/admin.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-admin',
   standalone: true,
-  template: `@if (isAdmin) {
-    <button>Admin button</button>
-    } @else {
-    <p>Not an admin</p>
-    }`,
+  templateUrl: `admin.component.html`,
 })
 export class AdminComponent {
-  isAdmin = false;
+  adminList = [
+    { name: 'Admin 1', age: 20 },
+    { name: 'Admin 2', age: 222 },
+    { name: 'Admin 3', age: 123 },
+  ];
 }
