d5b6d44be215fdb7c3befef26e1351c9033c9dbf
Alex Naltu <naltuok@gmail.com>
2024-10-19 07:13:43 +0200

    learned different rxjs methods

--- learning-angular/src/app/app.component.ts
+++ learning-angular/src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component, inject } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  input,
+  Signal,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { TodoListItemComponent } from './components/todo-list-item.component';
 import { SignUpFormComponent } from './components/sign-up-form.component';
@@ -9,6 +17,22 @@ import { DetailsComponent } from './components/details/details.component';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { PostsService } from './posts.service';
+import { FormsModule } from '@angular/forms';
+import {
+  concatMap,
+  delay,
+  exhaustMap,
+  from,
+  map,
+  mergeMap,
+  of,
+  switchMap,
+} from 'rxjs';
+
+interface Task {
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
@@ -24,8 +48,10 @@ import { PostsService } from './posts.service';
     RouterLink,
     RouterLinkActive,
     CommonModule,
+    FormsModule,
   ],
   templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'learning-angular';
@@ -40,7 +66,14 @@ export class AppComponent {
 
   posts: any[] = [];
 
+  array$ = from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).pipe(map((x) => x * 3));
+
   constructor() {
+    from([1, 2, 3, 4])
+      .pipe(exhaustMap((x) => of(x)))
+      .pipe(delay(1000))
+      .subscribe(console.log);
+
     this.postService.getPostsByParams(1).subscribe({
       next: (data: any) => {
         this.posts = data;
@@ -62,5 +95,35 @@ export class AppComponent {
           console.error('There was an error!', error);
         },
       });
+  }
+
+  newTask: string = '';
+  tasks: WritableSignal<Task[]> = signal([]);
+
+  // Computed signal to get the count of completed tasks
+  completedTasksCount: Signal<number> = computed(
+    () => this.tasks().filter((task) => task.completed).length
+  );
+
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.update((tasks) => [
+        ...tasks,
+        { title: this.newTask, completed: false },
+      ]);
+      this.newTask = ''; // Clear the input field
+    }
+  }
+
+  deleteTask(taskToDelete: Task) {
+    this.tasks.update((tasks) => tasks.filter((task) => task !== taskToDelete));
+  }
+
+  toggleTaskCompletion(taskToToggle: Task) {
+    this.tasks.update((tasks) =>
+      tasks.map((task) =>
+        task === taskToToggle ? { ...task, completed: !task.completed } : task
+      )
+    );
   }
 }
